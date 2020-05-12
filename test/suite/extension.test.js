5911e232e7c8e86a1f5798921ba5f9ee4c5d7e0c
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-05-11 23:42:27 -0700

    #8 更新测试

--- test/suite/extension.test.js
+++ test/suite/extension.test.js
@@ -6,7 +6,32 @@ const { before } = require('mocha');
 const vscode = require('vscode');
 // const 补全实现 = require('../../补全实现') // TODO: 应直接引用模块
 // 拷贝自"补全实现"
-const 标识符模式 = /(-?\d*\.\d\w*)|([^\`\~\!\@\^\&\*\(\)\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\s？。，、；：？…—·ˉˇ¨“”～〃｜《》〔〕（），]+)/g;
+const 标识符模式 = /(-?\d*\.\d\w*)|([^\`\~\!\@\^\&\*\(\)\-\#\?\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\s？。，、；：？…—·ˉˇ¨“”～〃｜《》〔〕（），]+)/g;
+const 待测代码 = [
+	"1+数?1",
+	"1+数+1",
+	"1+数-1",
+	"1+数*1",
+	"1+数/1",
+	"1+数\\1",
+	"1+数=1",
+	"1+数`1",
+	"1+数~1",
+	"1+数!1",
+	"1+数@1",
+	"1+数#1",
+	"1+数^1",
+	"1+数&1",
+	"1+数*1",
+	"1+数(1",
+	"1+数)1",
+	"1+数<1",
+	"1+数>1",
+	"1+数:1",
+	"1+数'1",
+	"1+数:1",
+	"1+数\"1",
+	]
 
 suite('Extension Test Suite', () => {
 	before(() => {
@@ -14,11 +39,18 @@ suite('Extension Test Suite', () => {
 	});
 
 	test('标识符识别', () => {
-		assert.deepEqual("量 - 1".match(标识符模式), ["量", "-", "1"]);
+		assert.deepEqual("量 - 1".match(标识符模式), ["量", "1"]);
 
-		// 复现 https://github.com/program-in-chinese/vscode_Chinese_Input_Assistant/issues/8
-		assert.deepEqual("量-1".match(标识符模式), ["量-1"], "现状有误");
-		// TODO: 下面应通过
-		assert.deepEqual("量-1".match(标识符模式), ["量", "-", "1"], "与-相连");
+		// https://github.com/program-in-chinese/vscode_Chinese_Input_Assistant/issues/8
+		assert.deepEqual("量-1".match(标识符模式), ["量", "1"], "与-相连");
+		for (代码 of 待测代码) {
+			assert.deepEqual(代码.match(标识符模式), ["1", "数", "1"], 代码);
+		}
+		assert.deepEqual("1+数.1".match(标识符模式), ["1", "数", ".1"], "1+数.1");
+		assert.deepEqual("数.方法".match(标识符模式), ["数", "方法"], "数.方法");
+
+		assert.deepEqual("1+数_1".match(标识符模式), ["1", "数_1"], "1+数_1");
+
+		assert.deepEqual("1+数$1".match(标识符模式), ["1", "数$1"], "1+数$1");
 	});
 });
